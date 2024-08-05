f065fb51e07a7ef68f93155637c31236a05d1833
Nikolay Marinov <nikomarinov@gmail.com>
2024-08-05 09:08:46 +0300

    1

--- backup/create_iso.sh
+++ backup/create_iso.sh
@@ -5,3 +5,4 @@ ocs-iso -l 0 -g en_US.UTF-8 -k NONE -a a1-clonezilla -i a1.v1 -u /a1/ -x noeject
 sshpass -p B@danarka100 scp -o StrictHostKeyChecking=no a1-clonezilla.iso n_marinov@192.168.99.1:/home/n_marinov/Downloads/
 
 
+
